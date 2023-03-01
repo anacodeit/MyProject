d3987f080ddeaffd87a0aa554ef2ba9b17718f71
Ars_ky <arskay1@gmail.com>
2023-03-01 19:11:29 +0400

    add all

---
+++ madlips3.py
@@ -0,0 +1,39 @@
+#i used the input function to get input from the user, then I assigned variables
+
+#some of the variables in the code are used multiple times
+
+noun_name = input("Give me a Noun or a Person's name: ")
+adjective_1 = input("Give me an Adjective: ")
+color = input("Give me a Color: ")
+animal = input("Name an Animal: ")
+place = input("Name a Place: ")
+adjective_2 = input("Give me another Adjective: ")
+Magical_Creature_1_plural = input("Tell me a Magical Creature (plural form): ")
+adjective_3 = input("Give an Adjective again: ")
+Magical_Creature_2_plural = input("Name a Magical Creature again (plural form): ")
+Room_House = input("Name a Room in a House: ")
+noun_1 = input("Give me a Noun: ")
+noun_2 = input("Give me another Noun: ")
+noun_3_plural = input("Tell me a Noun (plural form): ")
+adjective_4 = input("Write an Adjective once more: ")
+noun_plural_4 = input("Insert a Noun (plural form): ")
+number = input("Insert a Number: ")
+Measure_time = input("Measure of Time: ") #MISSING THE STORYLINE!!!
+verb_ing = input("Insert a Verb ending with 'ing': ")
+adjective_5 = input("Name an Adjective: ")
+noun_5 = input("Insert a Noun: ")
+
+madlips3 = f"""Dear {noun_name}, I am writing to you from a {adjective_1} castle in an enchanted forest. I found
+myself here one day after going for a ride on a {color} {animal} in {place} .There are {adjective_2}
+{Magical_Creature_1_plural} and {adjective_3} {Magical_Creature_2_plural} here! In the {Room_House} there is a pool 
+full of {noun_1}. I fall asleep each night on a {noun_2}  of {noun_3_plural} and dream of {adjective_4} {noun_plural_4}.
+It feels as though I have lived here for {number} {Measure_time}. I hope one day you can visit, although the only way to
+get here now is {verb_ing} on a {adjective_5}!
+
+
+
+
+"""
+
+print(madlips3)
+
